bbdf2eabd9e918dc3e9c1dbce7da936886d660e1
William Moon <william-moon@lambdastudents.com>
2021-03-24 14:32:10 -0700

    finished all tasks

--- index.js
+++ index.js
@@ -84,9 +84,17 @@ console.log(you.stomach);
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
-  }
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
+  };
+
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
+  };
+
   
   
   /*
@@ -96,9 +104,15 @@ console.log(you.stomach);
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
+  };
+
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
+  };
  
   
   /* 
